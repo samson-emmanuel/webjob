1d9828f1644f4adb521dd02e3f475147faab5ff3
samson emmanule <samsonemmanuel9025@gmail.com>
2023-12-18 16:04:07 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// section for the navigation
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
+
